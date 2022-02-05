431ddfc33e39dee57a36d60a459202fb811a2bb4
Najat MBODJI <najatmbodj@hotmail.fr>
2022-02-05 12:55:56 +0100

    create a directory for generated csv files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import CategoryPage
 from bs4 import BeautifulSoup
 import requests
 import csv
+import os
 
 
 def create_csv_file(nameFile):
@@ -16,9 +17,10 @@ def create_csv_file(nameFile):
 web_html_page = requests.get("https://books.toscrape.com/index.html")
 soup_web_html_page = BeautifulSoup(web_html_page.content, 'html.parser')
 all_html_category = soup_web_html_page.find('ul', class_='nav nav-list').ul.find_all('li')
-#alltheCategory = alltheCategory.ul.find_all('li')
+os.mkdir("csvFiles")
 for category in all_html_category:
     nameFile = category.a.string.strip() + ".csv"
+    nameFile = "csvFiles/" + nameFile
     create_csv_file(nameFile)
     print(category.a.string.strip())
     product_category_url = "https://books.toscrape.com/" + category.a["href"]
